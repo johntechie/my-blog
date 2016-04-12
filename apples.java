216ffa1572b556634bd28ec8d7b4d1ea956bea13
John Wasike <jwasike2@gmail.com>
2016-04-12 07:35:41 +0300

    commit all

---
+++ apples.java
@@ -0,0 +1,13 @@
+class apples {
+	public static void public static void main(String[] args) {
+		int age = 55;
+
+		if (age > 6)
+		   system.out.printIn("fuck you");
+
+		    else 
+		         system.out.printIn("go to hell");	
+			
+		}
+	}
+}
---
+++ player.java
@@ -0,0 +1,11 @@
+class player {
+	public static void public static void main(String[] args) {
+		String name = "leon";
+	    int lives = 3;
+		int score = 10;
+		int level = 1;
+
+
+	}
+system.out.printIn(person.getName());
+system.out.printIn()
